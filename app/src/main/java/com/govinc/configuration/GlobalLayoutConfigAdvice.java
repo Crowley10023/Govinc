1b83483de469db04bb055f5878ea1a8756871128
Crowley10023 <mail@keller-ip.de>
2025-10-16 16:41:50 +0200

    Update GlobalLayoutConfigAdvice.java

--- app/src/main/java/com/govinc/configuration/GlobalLayoutConfigAdvice.java
+++ app/src/main/java/com/govinc/configuration/GlobalLayoutConfigAdvice.java
@@ -14,9 +14,11 @@ public class GlobalLayoutConfigAdvice {
 
     @ModelAttribute("layoutConfig")
     public LayoutConfiguration loadLayoutConfig() {
-        LayoutConfiguration resu = layoutConfigurationRepository.findAll().stream().findFirst().orElse(null);
+        LayoutConfiguration resu = layoutConfigurationRepository.findAll().stream().findFirst().orElse(new LayoutConfiguration());
         System.out.println("checked\n\n\n" + resu);
-        System.out.println("PrimaryColor: " + resu.getPrimaryColor());
+        if (resu != null) {
+            System.out.println("PrimaryColor: " + resu.getPrimaryColor());
+        }
         return resu;
     }
 }
