0b81da327825f4c52c34f7b93c3d4c0db3a1f28e
Crowley10023 <mail@keller-ip.de>
2025-11-13 12:54:14 +0100

    Update OpenAIConfigController.java

--- app/src/main/java/com/govinc/controller/OpenAIConfigController.java
+++ app/src/main/java/com/govinc/controller/OpenAIConfigController.java
@@ -15,6 +15,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 import java.util.List;
+import java.util.Map;
 
 @Controller
 @RequestMapping("/config/openai")
